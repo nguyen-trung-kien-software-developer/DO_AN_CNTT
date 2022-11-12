9db050f766967c10e3015d80844ce7b46bcfeac4
Nguyen Trung Kien <111984477+nguyen-trung-kien-software-developer@users.noreply.github.com>
2022-11-12 13:15:32 +0700

    create model, fake data, repositories

--- doancntt/database/seeders/DatabaseSeeder.php
+++ doancntt/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // $this->call(CompanySeeder::class);
+        // $this->call(BannerSeeder::class);
+        // $this->call(PageSeeder::class);
+        // $this->call(IntroductionSeeder::class);
+        // $this->call(ParentCategorySeeder::class);
+        // $this->call(SubCategorySeeder::class);
+        // $this->call(BrandSeeder::class);
+        // $this->call(ProductSeeder::class);
+        // $this->call(BlogSeeder::class);
+        // $this->call(ConsultantSeeder::class);
+        // $this->call(HistorySeeder::class);
+        // $this->call(BranchSeeder::class);
+        // $this->call(ImageItemSeeder::class);
+        // $this->call(CommentSeeder::class);
+        // $this->call(DiscountCodeSeeder::class);
+        // $this->call(OrderStatusSeeder::class);
+        // $this->call(ShippingStatusSeeder::class);
+        // $this->call(CustomerSeeder::class);
+        // $this->call(UserSeeder::class);
+        // $this->call(RoleSeeder::class);
+        // $this->call(PermissionSeeder::class);
+        // $this->call(ProductBlogItemSeeder::class);
     }
 }
